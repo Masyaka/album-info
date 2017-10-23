9c7a4003cb4b6cd76bd1cad4f8e52e827c9b0c26
Maestroz <maestroz89@gmail.com>
2017-10-23 22:17:42 +0800

    Added local storage actions to AlbumListPage

--- app/components/AlbumsList.js
+++ app/components/AlbumsList.js
@@ -1,17 +1,48 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 
 import { GridList, GridTile } from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
 
-export default class AlbumsList extends Component {
+class AlbumTile extends PureComponent {
+  static propTypes = {
+    album: PropTypes.object.isRequired,
+    getActionIcon: PropTypes.func,
+  };
+
+  render() {
+    const {
+      album,
+      getActionIcon,
+    } = this.props;
+
+    return (
+      <GridTile
+        key={album.id}
+        title={album.title}
+        actionIcon={getActionIcon ? getActionIcon(album, () => this.forceUpdate()) : undefined}
+        actionPosition="left"
+        titlePosition="top"
+        titleBackground="black"
+        cols={1}
+        rows={2}
+        subtitle={album.description}
+        onClick={() => {
+        }}
+      />
+    );
+  }
+}
+
+export default class AlbumsList extends PureComponent {
 
   static propTypes = {
     albums: PropTypes.array,
+    getActionIcon: PropTypes.func,
   };
 
   render() {
     const {
       albums = [],
+      getActionIcon,
     } = this.props;
 
     return (
@@ -21,23 +52,7 @@ export default class AlbumsList extends Component {
           cellHeight={150}
           padding={16}
         >
-          {albums.map( (album) => {
-            return(
-              <GridTile
-                key={album.id}
-                title={album.title}
-                actionIcon={<IconButton/>}
-                actionPosition="left"
-                titlePosition="top"
-                titleBackground="black"
-                cols={1}
-                rows={2}
-                subtitle={album.description}
-                onClick={() => {
-                }}
-              />
-            );
-          })}
+          {albums.map( (album) => <AlbumTile key={album.id} album={album} getActionIcon={getActionIcon}/>)}
         </GridList>
       </div>
     );
--- app/pages/AlbumSearchPage.js
+++ app/pages/AlbumSearchPage.js
@@ -1,15 +1,73 @@
 import React, { Component } from 'react';
 
 import namedSection from '../mixins/namedSection';
+import lss from '../services/localStorageService';
 import AlbumSearchContainer from '../containers/AlbumSearchContainer';
 import AlbumSearchResultContainer from '../containers/AlbumSearchResultContainer';
 
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+
 export class AlbumSearchPage extends Component {
+
+  static STORED_ALBUMS_KEY = 'storedAlbums';
+
+  constructor(props) {
+    super(props);
+    if(!lss.get(AlbumSearchPage.STORED_ALBUMS_KEY)) {
+      lss.set(AlbumSearchPage.STORED_ALBUMS_KEY, {});
+    }
+  }
+
   render() {
     return (
       <div className="ph+">
         <AlbumSearchContainer/>
-        <AlbumSearchResultContainer/>
+        <AlbumSearchResultContainer
+          getActionIcon={ (album, onActionExecute) => {
+            const storedAlbumsMap = lss.get(AlbumSearchPage.STORED_ALBUMS_KEY);
+            if(storedAlbumsMap[album.id]) {
+              return (
+                <IconButton
+                  onTouchTap={ () => {
+                    const newStoredAlbums = lss.get(AlbumSearchPage.STORED_ALBUMS_KEY);
+                    delete newStoredAlbums[album.id];
+                    lss.set(
+                      AlbumSearchPage.STORED_ALBUMS_KEY,
+                      newStoredAlbums
+                    );
+                    onActionExecute();
+                  }}
+                >
+                  <FontIcon
+                    className="material-icons"
+                    color="white"
+                  >
+                    delete
+                  </FontIcon>
+                </IconButton>
+              );
+            }
+            return (
+              <IconButton
+                onTouchTap={ () => {
+                  lss.set(
+                    AlbumSearchPage.STORED_ALBUMS_KEY,
+                    {...lss.get(AlbumSearchPage.STORED_ALBUMS_KEY), [album.id]: album}
+                  );
+                  onActionExecute();
+                }}
+              >
+                <FontIcon
+                  className="material-icons"
+                  color="white"
+                >
+                  file_download
+                </FontIcon>
+              </IconButton>
+            );
+          }}
+        />
       </div>
     );
   }
