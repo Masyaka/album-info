ae57646aa8f9840d3dd53a90c1b5bb10d1b7e579
Maestroz <maestroz89@gmail.com>
2017-10-23 17:26:28 +0800

    Added searchAlbums and setApplicationHeader actions.

---
+++ app/actions/index.js
@@ -0,0 +1,16 @@
+import { createAction } from 'redux-act';
+
+import { get } from './fetch';
+
+export const albumSearchSuccess = createAction('albumSearchSuccess');
+
+export function searchAlbums(searchString) {
+  return async dispatch => {
+    get(`release?query=${searchString}&type=album&fmt=json`)
+      .then(json =>
+        dispatch(albumSearchSuccess(json))
+      );
+  };
+}
+
+export const setApplicationHeader = createAction('setApplicationHeader');
